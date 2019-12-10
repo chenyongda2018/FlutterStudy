e6a786bf6304edc1b3f95ee8240ac3d8fae925a9
chenyongda1 <chenyongda1@xiaomi.com>
2019-12-10 18:31:56 +0800

    add a statefulWidget.

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,9 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:test_flutter/widget/favorite_widget.dart';
 
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     //标题栏
     Widget titleSection = new Container(
       padding: EdgeInsets.all(32.0),
@@ -17,7 +14,7 @@ class MyHome extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   new Container(
-                    padding : const EdgeInsets.only(bottom: 8.0),
+                    padding: const EdgeInsets.only(bottom: 8.0),
                     child: new Text(
                       'Oeschinen Lake Campground',
                       style: new TextStyle(
@@ -46,7 +43,7 @@ class MyHome extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           new GestureDetector(
-            onTap :() {
+            onTap: () {
               Navigator.pushNamed(context, label);
             },
             child: new Icon(icon, color: color),
@@ -58,7 +55,6 @@ class MyHome extends StatelessWidget {
               style: new TextStyle(
                   fontSize: 12.0, fontWeight: FontWeight.w400, color: color),
             ),
-
           )
         ],
       );
@@ -80,7 +76,7 @@ class MyHome extends StatelessWidget {
 
     //文本部分
     Widget textArea = new Container(
-      padding: const EdgeInsets.all(32.0) ,
+      padding: const EdgeInsets.all(32.0),
       child: new Text(
         '''
 Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
@@ -90,23 +86,30 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
     );
 
     return new Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: new AppBar(
         title: Text('详情布局'),
       ),
-      body: new Column(
-        children: <Widget>[
-          new Image.asset(
-            'images/lake.jpg',
-            width: 400.0,
-            height:240.0,
-            fit: BoxFit.cover,
-          ),
-          titleSection,
-          buttonSection,
-          textArea,
-        ],
+      body: new Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child:new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Image.asset(
+                'images/lake.jpg',
+                width: 400.0,
+                height: 240.0,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textArea,
+            ],
+          ) ,
+        ),
       ),
     );
   }
-
 }
